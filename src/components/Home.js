6a8d9d3c39f868d1d1acfc2ac4ae65bb7552058d
leroyds <leroyds@live.com>
2020-09-05 13:14:34 +0530

    added useHomeFetch Hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../config';
 
 // import components
 import HeroImage from './elements/HeroImage';
@@ -8,18 +9,27 @@ import MovieThumb from './elements/MovieThumb';
 import LoadMoreBtn from './elements/LoadMoreBtn';
 import Spinner from './elements/Spinner';
 
+// import custom hooks
+import {useHomeFetch} from './Hooks/useHomeFetch';
 
-const Home = () => (
 
-    <>
-        <HeroImage/>
-        <SearchBar/>
-        <Grid/>
-        <MovieThumb/>
-        <Spinner/>
-        <LoadMoreBtn/>
-    </>
+const Home = () => {
 
-)
+    const [{state, loading, error}, fetchMovies] = useHomeFetch();
+    console.log(state);
+    
+
+    return(
+        <>
+            <HeroImage/>
+            <SearchBar/>
+            <Grid/>
+            <MovieThumb/>
+            <Spinner/>
+            <LoadMoreBtn/>
+        </>
+    )
+
+}
 
 export default Home;
--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 // Read more about the API here: https://developers.themoviedb.org/
 
 const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
+const API_KEY = '7dc5213fc1fc8a2fc07c5097ec986e58';
 const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
 // Sizes: w300, w780, w1280, original
 const BACKDROP_SIZE = 'w1280';
